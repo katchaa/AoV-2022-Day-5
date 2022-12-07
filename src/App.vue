2742a47969034e20e2405ef2dba183fe3f1257ed
Katcha <kratochvilova93@email.cz>
2022-12-07 03:18:43 +0100

    Task solution

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,34 @@
           <img src="/assets/santa.svg" alt="" class="max-w-[12em]" />
           <div>
             <h1 class="font-serif text-4xl">Happy Holidays!</h1>
-            <div class="mt-4 bg-white text-black p-2 rounded">TO: John</div>
-            <div class="mt-2 bg-white text-black p-2 rounded">FROM: Joana</div>
+            <div class="mt-4 bg-white text-black p-2 rounded">TO: {{ to }}</div>
+            <div class="mt-2 bg-white text-black p-2 rounded">FROM: {{ from }}</div>
           </div>
         </div>
       </div>
+      <form class="flex flex-col gap-2 mt-5">
+        <label for="to" class="w-full -mb-2">To: </label>
+        <input
+          v-model="to"
+          type="text"
+          id="to"
+          class="bg-gray-50 border p-2 rounded-lg text-sm focus:outline-gray-300"
+        />
+        <label for="from" class="w-full -mb-2">From: </label>
+        <input
+          v-model="from"
+          type="text"
+          id="from"
+          class="bg-gray-50 border p-2 rounded-lg text-sm focus:outline-gray-300"
+        />
+      </form>
     </div>
   </div>
 </template>
 
 <script setup>
-// ...
+import { ref } from 'vue'
+
+const to = ref('')
+const from = ref('')
 </script>
